92fb3bb7ef10fc31976dbae5351ec70949a7cc42
Alejandro <LuisAlejandroVM@hotmail.com>
2021-08-06 12:02:02 -0500

    Clase 02

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,14 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController{
 
-    public function getUsers(): Response{
-        return $this->json([
-            'message' => 'Listado de Usuarios',
-            'path' => 'src/Controller/UserController.php',
+    public function getUsers(){
+        $em = $this->getDoctrine()->getManager();
+        $listUsers = $em->getRepository('App:Users')->findBy([], ['name' => 'ASC']);
+        return $this->render('user/users.html.twig', [
+            'listUsers' => $listUsers
         ]);
+    }
+
+    public function createUser(Request $request){
+        $em = $this->getDoctrine()->getManager();
+
+        $users = new \App\Entity\Users();
+
+        $form_users = $this->createForm(\App\Form\UserType::class, $users);
+        $form_users->handleRequest($request);
+
+        if($form_users->isSubmitted() && $form_users->isValid()){
+            // Create user
+        }
+
+        // do redirection
     }
 }
