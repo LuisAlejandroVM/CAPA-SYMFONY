d02f19ffaf8eec52afa2c93c0468cbcf9bcc8b77
Alejandro <LuisAlejandroVM@hotmail.com>
2021-08-19 11:05:13 -0500

    get pdf

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+// Importamos html2pdf
+use Spipu\Html2Pdf\Html2Pdf;
+
 class UserController extends AbstractController{
 
     public function getUsers(){
@@ -68,5 +71,38 @@ class UserController extends AbstractController{
         $em->flush();
         
         return $this->redirectToRoute('getUsers');
+    }
+
+    public function getPDF(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $listUsers = $em->getRepository('App:Users')->findBy([], ['name' => 'ASC']);
+
+        ob_start();
+
+        $html = ob_get_clean();
+        
+        $html = $this->renderView('reports/users.html.twig', [
+            'listUsers' => $listUsers,
+            'actualDate' => ''
+        ]);
+
+        $html2pdf = new Html2Pdf('P', 'LETTER', 'fr', true, 'UTF-8', array('10', '10', '10', '10'));
+        $html2pdf->pdf->SetDisplayMode('real');
+        $html2pdf->setDefaultFont('helvetica');
+        $html2pdf->writeHTML($html);
+
+        $cadena = 'Users.pdf';
+        $originales = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕ';
+        $modificadas = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr';
+
+        $cadena = utf8_decode($cadena);
+        $cadena = strtr($cadena, utf8_decode($originales), $modificadas);
+        $cadena = strtoupper($cadena);
+
+        ob_end_clean();
+
+        return new Response($html2pdf->Output(utf8_encode($cadena), 'D'), 200, [
+            'Content-Type' => 'application/pdf;charset=UTF-8'
+        ]);
     }
 }
